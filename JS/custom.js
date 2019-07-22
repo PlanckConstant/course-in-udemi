8d45aaf804ba5a1ee8fdcafa723fe2a109a6afc4
Alex <i.s.k.a.n.d.e.r@mail.ru>
2019-07-22 23:35:26 +0700

    homework #1

--- JS/custom.js
+++ JS/custom.js
@@ -1,15 +1,22 @@
-"use strict";
+let money = prompt("Ваш бюджет на месяц?","");
+let time = prompt("Введите дату в формате YYYY- MM-DD","");
 
-let = "money";
-let = "time";
+let appData = {
+    money,                 //бюджет
+    time,                  //данные времени
+    expenses: {
+        answer1: prompt("Введите обязательную статью расходов в этом месяце"),
+        answer2: prompt("Во сколько обойдется?")
+    },                     //объект с обязательными расходами
+    optionalExpenses: {},  //объект с необязательными расходами
+    income: [],            //массив данных с доп. доходом
+    savings: false         //свойство
+};
 
-money = prompt("Ваш бюджет на месяц?");
-time = prompt("Введите дату в формате YYYY- MM-DD");l
-appData = {
-    money,                  //бюджет
-    time,                   //данные времени
-    expenses = {},          //объект с обязательными расходами
-    optionalExpenses = {},  //объект с необязательными расходами
-    income = [],            //массив данных с доп. доходом
-    savings = false         //свойство
-}
+// console.log(appData);   
+function dailyBudget() {
+    let i = (+money - +appData.expenses.answer2)/30;
+    alert(i);
+};
+
+dailyBudget();
