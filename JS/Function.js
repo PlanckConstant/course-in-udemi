e9bf7d08275ec079d870b553aa253902df6e7ac4
Alex <i.s.k.a.n.d.e.r@mail.ru>
2019-08-11 23:23:47 +0700

    Function =>
    Draft

--- JS/Function.js
+++ JS/Function.js
@@ -8,8 +8,35 @@
 // showFirstMessage("Hello world");
 // console.log(num);
 
-function calc(a,b){
-    return(a+b);
-}
-console.log(calc(3,4));
+// function calc(a,b){ // Деклоративная функция. Можно вызывать до объявлени. 
+//     return(a+b);
+// }
+
+// let calc  = function(a,b){ // Функциональное выражение. Работает только после объявлени. 
+//     return(a+b);
+// }
+
+let calc = (a,b) => (a+b); // Стрелочная функция!!!
+console.log(calc(3,4)); // вызов функции с параметрами
 console.log(calc(91,9));
+
+function retVar() {
+    let num = 50;
+    return num; // возврат значения функции во внешний мир
+;}
+
+console.log(retVar); // Обращение к функциии
+let anotherNum = retVar(); // вытаскиваем значение локальной переменной.
+console.log(anotherNum);
+
+let str = "Test";
+console.log(str.length); //Метод возвращающий длину строки
+console.log(str.toUpperCase()); // метот больших букв строк
+console.log(str.toLowerCase()); // метод маленькихбукв строк    
+
+let twelve = "12.21314";
+console.log(Math.round(twelve));    // Округление до целых
+
+let twelve = "12.2px";
+console.log(parseInt(twelve));  
+console.log(parseFloat(twelve)); 
