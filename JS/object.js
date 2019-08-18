67f2c56c6771279f1aaf1c2b9f6116f43de12b22
Alex <i.s.k.a.n.d.e.r@mail.ru>
2019-08-19 01:08:05 +0700

    sort strings & numbers

--- JS/object.js
+++ JS/object.js
@@ -20,21 +20,29 @@
 
     console.log(Object.keys(options).length); // показывает количество ключей в объекте
 
-    let arr = [1, 2, 3, 4, 5];
+    function forEach () {
+        let arr = [1, 2, 3, 4, 5];
 
-    arr.forEach(function(item, i, mass) {   // forEach перебор массива
-        console.log(i + ': ' + item + " (массив: " +mass + ")");
-    });
-
-
-    let mass = [1,3,4,6,7];
-
-    for (let key in mass) { // цикл для вывода ключей массива
-        console.log (key);
+        arr.forEach(function(item, i, mass) {   // forEach перебор массива
+            console.log(i + ': ' + item + " (массив: " +mass + ")");
+        });
     };
-    for (let key of mass) { // цикл для вывода значений массива
-        console.log (key);
+
+    // forEach();
+
+    function forInOf () {
+        let mass = [1,3,4,6,7];
+
+        for (let key in mass) { // цикл для вывода ключей массива
+            console.log (key);
+        };
+        for (let key of mass) { // цикл для вывода значений массива
+            console.log (key);
+        };
     };
+
+    // forInOf();
+
 
     function fun1() {
         arr.pop();      // метод .pop удаляет последний элемент массива
@@ -51,3 +59,40 @@
     };
     
     //fun1();
+
+    function split (){
+        let ans = prompt("", ""),
+            arr = [];
+
+        arr = ans.split(',');   // метод .split записывает ответы в массив
+        console.log(arr);
+    };
+
+    // split();
+
+    function join (){
+        let arr = ["qwe", "rty", "uio", "op["];
+            i = arr.join(", ");         // метод .join склеивает массив в строку через разделитель
+        
+        console.log((typeof(i)) +" - " + i);
+    };
+
+
+
+    function join (){
+        let arr = ["qwe", "rty", "uio", "op["];
+            i = arr.sort();         // метод .sort сортирует массив
+        
+        console.log(arr);
+    };
+
+
+    function joinToNumber () {
+        let arr = [1,35,75,2,4,14];
+            i = arr.sort(compareNum);         // метод .sort сортирует массив как строку
+
+        function compareNum(a,b) {       // так можно заставить упорядочивать по значению, а не строке. ХЗ как работает. Сравнивает каждую пару.
+            return a-b;
+        };
+    console.log(arr);  
+    }
