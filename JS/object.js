718c9c8de2a34775211ffef60f9b914f9ef3d1de
Alex <i.s.k.a.n.d.e.r@mail.ru>
2019-08-18 19:43:15 +0700

    objects##

--- JS/object.js
+++ JS/object.js
@@ -19,4 +19,26 @@
     };
 
     console.log(Object.keys(options).length); // показывает количество ключей в объекте
+
+    let arr = [1, 2, 3, 4, 5];
+
+    arr.forEach(function(item, i, mass) {   // forEach перебор массива
+        console.log(i + ': ' + item + " (массив: " +mass + ")");
+    });
+
+
+    function fun1() {
+        arr.pop();      // метод .pop удаляет последний элемент массива
+        arr.push('5')   // метод .push добавляет в конце новый элемент
+        arr.shift();    // метот .shift удаляет первый элемент у массива
+        arr.unshift('1');// метод .unshift добавляет первый элемент массиву
+
+        for (let i = 0; i<arr.length; i++) {
+            console.log(typeof(arr[i])+ ' ' + arr[i]);
+        }
+        arr[99] = 'x';
+        console.log(arr);
+        console.log(arr.length);    // == 100 | lenght возвращает не количество элементов, а последний индекс + 1
+    };
     
+    //fun1();
